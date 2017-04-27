83746f88c41110895a88acfefdeab1590ea62efe
mkalenicz <maciej.kalenicz@gmail.com>
2017-04-27 18:10:26 +0200

    ttt

--- src/main/java/pl/ttt/Remainder.java
+++ src/main/java/pl/ttt/Remainder.java
@@ -6,9 +6,14 @@ import java.util.List;
  * Created by RENT on 2017-11-30.
  */
 public class Remainder {
-
     public List<Integer> getChange(int amount){
-
         return null;
+    }
+    public static int Change {
+       int cash = 0;
+        if (cash > 500 ) {
+            return
+        }
+            return ;
     }
 }
