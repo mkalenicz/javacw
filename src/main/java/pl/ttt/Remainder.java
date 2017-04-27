141d56d6563adb46030d0a08d4d13c44aec77b77
mkalenicz <maciej.kalenicz@gmail.com>
2017-04-27 18:40:21 +0200

    tttfff

--- src/main/java/pl/ttt/Remainder.java
+++ src/main/java/pl/ttt/Remainder.java
@@ -1,31 +1,27 @@
 package pl.ttt;
 
 import java.util.List;
+import java.util.ArrayList;
 
 /**
  * Created by RENT on 2017-11-30.
  */
 public class Remainder {
     public List<Integer> getChange(int amount){
-        return null;
-    }
-    public static int Change {
-       int cash = 0;
-        int cash500 = 0;
-       int cash200 = 0;
-       int cash100 = 0;
-       int cash50 = 0;
-       int cash20 = 0;
-       int cash10 = 0;
-       int cash5  = 0;
-        int cash2  = 0;
-        int cash1  = 0;
-edfwf
-        if (cash / 500 == 0) {
-        int cash500 = 1;
-        } else {
+        ArrayList<Integer>  nominals = new ArrayList<Integer>();
+        list.add(500);
+        list.add(200);
+        list.add(100);
+        list.add(50);
+        list.add(20);
+        list.add(10);
+        list.add(5);
+
+        if (amount > 0) {
 
         }
-            return ;
+
+        return null;
     }
+
 }
