0fe6563d64995628dfe862e969409000e769b259
mkalenicz <maciej.kalenicz@gmail.com>
2017-04-27 18:19:02 +0200

    ttt

--- src/main/java/pl/ttt/Remainder.java
+++ src/main/java/pl/ttt/Remainder.java
@@ -11,8 +11,20 @@ public class Remainder {
     }
     public static int Change {
        int cash = 0;
-        if (cash > 500 ) {
-            return
+        int cash500 = 0;
+       int cash200 = 0;
+       int cash100 = 0;
+       int cash50 = 0;
+       int cash20 = 0;
+       int cash10 = 0;
+       int cash5  = 0;
+        int cash2  = 0;
+        int cash1  = 0;
+
+        if (cash / 500 == 0) {
+        int cash500 = 1;
+        } else {
+
         }
             return ;
     }
