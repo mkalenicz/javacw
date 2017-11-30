7fbc9e9f34e2112adb3d1b84580b6573e85c16aa
zjawaWro <javawro3@gmail.com>
2017-11-30 18:02:05 +0100

    2 zmiana

--- src/main/java/pl/ttt/App.java
+++ src/main/java/pl/ttt/App.java
@@ -4,10 +4,14 @@ package pl.ttt;
  * Hello world!
  *
  */
-public class App 
-{
+public class App {
+
     public static void main( String[] args )
     {
         System.out.println( "Hello Wroclaw!" );
+    }
+
+    public static void rest (int value){
+
     }
 }
---
+++ src/main/java/pl/ttt/Remainder.java
@@ -0,0 +1,14 @@
+package pl.ttt;
+
+import java.util.List;
+
+/**
+ * Created by RENT on 2017-11-30.
+ */
+public class Remainder {
+
+    public List<Integer> getChange(int amount){
+
+        return null;
+    }
+}
