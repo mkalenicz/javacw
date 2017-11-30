80ce5aa4aa469a2c87a058bca7611c0928d2fe81
zjawaWro <javawro3@gmail.com>
2017-11-30 18:25:39 +0100

    3 zmiana

---
+++ src/test/java/pl/ttt/RemainderTest.java
@@ -0,0 +1,31 @@
+package pl.ttt;
+
+import org.junit.Test;
+
+import java.lang.reflect.Array;
+import java.util.*;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by RENT on 2017-11-30.
+ */
+public class RemainderTest {
+
+    @Test
+    public void getChange_Test1(){
+        //
+        int amount = 300;
+
+        //
+        List<Integer> changeList = new ArrayList<Integer>();
+        changeList.add(200);
+        changeList.add(100);
+
+        //
+        assertEquals();
+    }
+
+
+
+}
